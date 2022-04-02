bdbc42a096c613625883133840ca16612985f26d
Praditya Raudi <pradityaraudi@gmail.com>
2022-04-02 15:24:45 -0400

    remove border column from download button

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,7 +61,7 @@ def display_result(data_frame, labels, statuses, datetimes, uuids):
     # Made it this way to uniquely set the css
     with st.container():
         with st.container():
-            _, col2 = st.columns([2, 1])
+            _, col2 = st.columns([5, 2])
             with col2:
                 dl_button = st.empty()
    
